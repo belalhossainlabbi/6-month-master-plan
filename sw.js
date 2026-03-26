c732860dd812212fb192a848d59f6c986196317f
belalhossainlabbi <contact.with.belal.hossain@gmail.com>
2026-03-26 15:18:20 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "my-app-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(["/", "/index.html"]))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
